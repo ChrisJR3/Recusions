11dd9d1f1ed4c2528065aaf7f8e022c1bd91f2ce
Tony Theodoropoulos <theodot@outlook.com>
2020-05-07 10:48:00 -0400

    Add project files.

---
+++ Recusions/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Recusions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.factorialButton = new System.Windows.Forms.Button();
+            this.factorialInput = new System.Windows.Forms.TextBox();
+            this.factorialOutput = new System.Windows.Forms.Label();
+            this.gcdOutput = new System.Windows.Forms.Label();
+            this.gcd1Input = new System.Windows.Forms.TextBox();
+            this.gcd2Input = new System.Windows.Forms.TextBox();
+            this.gcdButton = new System.Windows.Forms.Button();
+            this.hanoiButton = new System.Windows.Forms.Button();
+            this.hanoiOutput = new System.Windows.Forms.Label();
+            this.scrambleInput = new System.Windows.Forms.TextBox();
+            this.scrambleButton = new System.Windows.Forms.Button();
+            this.scrambleOutput = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lineLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // factorialButton
+            // 
+            this.factorialButton.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.factorialButton.Location = new System.Drawing.Point(191, 39);
+            this.factorialButton.Name = "factorialButton";
+            this.factorialButton.Size = new System.Drawing.Size(86, 43);
+            this.factorialButton.TabIndex = 0;
+            this.factorialButton.Text = "Factorial";
+            this.factorialButton.UseVisualStyleBackColor = true;
+            this.factorialButton.Click += new System.EventHandler(this.factorialButton_Click);
+            // 
+            // factorialInput
+            // 
+            this.factorialInput.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.factorialInput.Location = new System.Drawing.Point(112, 49);
+            this.factorialInput.Name = "factorialInput";
+            this.factorialInput.Size = new System.Drawing.Size(55, 25);
+            this.factorialInput.TabIndex = 1;
+            // 
+            // factorialOutput
+            // 
+            this.factorialOutput.BackColor = System.Drawing.Color.White;
+            this.factorialOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.factorialOutput.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.factorialOutput.Location = new System.Drawing.Point(40, 95);
+            this.factorialOutput.Name = "factorialOutput";
+            this.factorialOutput.Size = new System.Drawing.Size(237, 26);
+            this.factorialOutput.TabIndex = 2;
+            this.factorialOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gcdOutput
+            // 
+            this.gcdOutput.BackColor = System.Drawing.Color.White;
+            this.gcdOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gcdOutput.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gcdOutput.Location = new System.Drawing.Point(40, 236);
+            this.gcdOutput.Name = "gcdOutput";
+            this.gcdOutput.Size = new System.Drawing.Size(237, 28);
+            this.gcdOutput.TabIndex = 3;
+            this.gcdOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gcd1Input
+            // 
+            this.gcd1Input.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gcd1Input.Location = new System.Drawing.Point(112, 170);
+            this.gcd1Input.Name = "gcd1Input";
+            this.gcd1Input.Size = new System.Drawing.Size(55, 25);
+            this.gcd1Input.TabIndex = 4;
+            // 
+            // gcd2Input
+            // 
+            this.gcd2Input.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gcd2Input.Location = new System.Drawing.Point(112, 198);
+            this.gcd2Input.Name = "gcd2Input";
+            this.gcd2Input.Size = new System.Drawing.Size(55, 25);
+            this.gcd2Input.TabIndex = 5;
+            // 
+            // gcdButton
+            // 
+            this.gcdButton.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gcdButton.Location = new System.Drawing.Point(191, 170);
+            this.gcdButton.Name = "gcdButton";
+            this.gcdButton.Size = new System.Drawing.Size(86, 50);
+            this.gcdButton.TabIndex = 6;
+            this.gcdButton.Text = "GCD";
+            this.gcdButton.UseVisualStyleBackColor = true;
+            this.gcdButton.Click += new System.EventHandler(this.gcdButton_Click);
+            // 
+            // hanoiButton
+            // 
+            this.hanoiButton.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hanoiButton.Location = new System.Drawing.Point(366, 39);
+            this.hanoiButton.Name = "hanoiButton";
+            this.hanoiButton.Size = new System.Drawing.Size(86, 43);
+            this.hanoiButton.TabIndex = 7;
+            this.hanoiButton.Text = "Hanoi";
+            this.hanoiButton.UseVisualStyleBackColor = true;
+            this.hanoiButton.Click += new System.EventHandler(this.hanoiButton_Click);
+            // 
+            // hanoiOutput
+            // 
+            this.hanoiOutput.BackColor = System.Drawing.Color.White;
+            this.hanoiOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.hanoiOutput.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hanoiOutput.Location = new System.Drawing.Point(362, 99);
+            this.hanoiOutput.Name = "hanoiOutput";
+            this.hanoiOutput.Size = new System.Drawing.Size(239, 417);
+            this.hanoiOutput.TabIndex = 8;
+            // 
+            // scrambleInput
+            // 
+            this.scrambleInput.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scrambleInput.Location = new System.Drawing.Point(112, 319);
+            this.scrambleInput.Name = "scrambleInput";
+            this.scrambleInput.Size = new System.Drawing.Size(56, 25);
+            this.scrambleInput.TabIndex = 9;
+            // 
+            // scrambleButton
+            // 
+            this.scrambleButton.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scrambleButton.Location = new System.Drawing.Point(188, 309);
+            this.scrambleButton.Name = "scrambleButton";
+            this.scrambleButton.Size = new System.Drawing.Size(86, 43);
+            this.scrambleButton.TabIndex = 10;
+            this.scrambleButton.Text = "Scramble";
+            this.scrambleButton.UseVisualStyleBackColor = true;
+            this.scrambleButton.Click += new System.EventHandler(this.scrambleButton_Click);
+            // 
+            // scrambleOutput
+            // 
+            this.scrambleOutput.BackColor = System.Drawing.Color.White;
+            this.scrambleOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scrambleOutput.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scrambleOutput.Location = new System.Drawing.Point(40, 368);
+            this.scrambleOutput.Name = "scrambleOutput";
+            this.scrambleOutput.Size = new System.Drawing.Size(237, 148);
+            this.scrambleOutput.TabIndex = 11;
+            this.scrambleOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 19);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Number 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 19);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Number 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 19);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Factorial";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(39, 309);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 46);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "String (max 4)";
+            // 
+            // lineLabel
+            // 
+            this.lineLabel.BackColor = System.Drawing.Color.Black;
+            this.lineLabel.Location = new System.Drawing.Point(315, 39);
+            this.lineLabel.Name = "lineLabel";
+            this.lineLabel.Size = new System.Drawing.Size(10, 477);
+            this.lineLabel.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 554);
+            this.Controls.Add(this.lineLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.scrambleOutput);
+            this.Controls.Add(this.scrambleButton);
+            this.Controls.Add(this.scrambleInput);
+            this.Controls.Add(this.hanoiOutput);
+            this.Controls.Add(this.hanoiButton);
+            this.Controls.Add(this.gcdButton);
+            this.Controls.Add(this.gcd2Input);
+            this.Controls.Add(this.gcd1Input);
+            this.Controls.Add(this.gcdOutput);
+            this.Controls.Add(this.factorialOutput);
+            this.Controls.Add(this.factorialInput);
+            this.Controls.Add(this.factorialButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Recursion Examples";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button factorialButton;
+        private System.Windows.Forms.TextBox factorialInput;
+        private System.Windows.Forms.Label factorialOutput;
+        private System.Windows.Forms.Label gcdOutput;
+        private System.Windows.Forms.TextBox gcd1Input;
+        private System.Windows.Forms.TextBox gcd2Input;
+        private System.Windows.Forms.Button gcdButton;
+        private System.Windows.Forms.Button hanoiButton;
+        private System.Windows.Forms.Label hanoiOutput;
+        private System.Windows.Forms.TextBox scrambleInput;
+        private System.Windows.Forms.Button scrambleButton;
+        private System.Windows.Forms.Label scrambleOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lineLabel;
+    }
+}
+
---
+++ Recusions/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recusions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void factorialButton_Click(object sender, EventArgs e)
+        {
+            int number;
+
+            try
+            {
+                number = Convert.ToInt32(factorialInput.Text);
+            }
+            catch
+            {
+                return;
+            }
+
+            int result = Factorial(number);
+            factorialOutput.Text = $"The factorial of {number} is {result}";
+        }
+
+        private void gcdButton_Click(object sender, EventArgs e)
+        {
+            int num1, num2;
+
+            try
+            {
+                num1 = Convert.ToInt32(gcd1Input.Text);
+                num2 = Convert.ToInt32(gcd2Input.Text);
+            }
+            catch
+            {
+                return;
+            }
+
+            int result = GCD(num1, num2);
+
+            gcdOutput.Text = $"The GCD of {num1} and {num2} is {result}";
+        }
+
+        private void hanoiButton_Click(object sender, EventArgs e)
+        {
+            int discs = 4;
+
+            hanoiOutput.Text = $"There are {discs} discs on peg A initially \n\n";
+
+            Hanoi("A", "C", "B", discs);
+        }
+
+        private void scrambleButton_Click(object sender, EventArgs e)
+        {
+            string letters = scrambleInput.Text;
+
+            scrambleOutput.Text = $"Starting letters are {letters} \n\n";
+
+            Scramble("", letters);
+        }
+
+        public int Factorial(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return (n * Factorial(n - 1));
+            }
+        }
+
+        public int GCD(int a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            else
+            {
+                return GCD(b, a % b);
+            }
+        }
+
+
+        public void Hanoi(string from, string to, string temp, int n)
+        {
+            if (n == 1)
+            {
+                hanoiOutput.Text += $"Move disc 1 from {from} to {to} \n";
+            }
+            else
+            {
+                Hanoi(from, temp, to, n - 1);
+                hanoiOutput.Text += $"Move disc {n} from {from} to {to} \n";
+                Hanoi(temp, to, from, n - 1);
+            }
+        }
+
+        public void Scramble(string startString, string endString)
+        {
+            string newString;
+
+            if (endString.Length == 1)
+            {
+                scrambleOutput.Text += startString + endString + "  ";
+            }
+            else
+            {
+                for (int i = 0; i < endString.Length; i++)
+                {
+                    newString = endString.Substring(0, i) + endString.Substring(i + 1);
+                    Scramble(startString + endString.ElementAt(i), newString);
+                }
+            }
+        }
+    }
+}
